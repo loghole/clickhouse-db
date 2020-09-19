84b4200504dc83198db26ee5550cbe1702c18357
lissteron <lissteron@gmail.com>
2020-09-19 17:33:49 +0300

    add row_id

--- init.sql
+++ init.sql
@@ -17,10 +17,11 @@ CREATE TABLE IF NOT EXISTS logs.internal_logs (
     `params_float.keys` Array(String),
     `params_float.values` Array(Float64),
     `build_commit` String,
-    `config_hash` String
+    `config_hash` String,
+    `row_id` UInt64
 ) ENGINE = MergeTree()
       PARTITION BY (date)
       ORDER BY (time, namespace, source)
-      SETTINGS index_granularity = 8192;
+      SETTINGS index_granularity = 1024;
 
 CREATE TABLE IF NOT EXISTS logs.internal_logs_buffer AS logs.internal_logs ENGINE = Buffer(logs, internal_logs, 16, 10, 100, 10000, 1000000, 10000000, 100000000);
